1fbb2481c863b5f66095c154d62cd3ce0331857c
svelde <svelde@gmail.com>
2021-04-25 20:59:01 +0200

    single proejct

--- src/icam/TriggeredImageTakerApp/Icam7000.cs
+++ src/icam/TriggeredImageTakerApp/Icam7000.cs
@@ -379,14 +379,14 @@ namespace TriggeredImageTakerApp
             //Console.WriteLine("buffer size = 0x{0:X} \n", evtimage.ImageSize);
 
             //Step13: Get the image buffer base address
-            //byte[] byBuffer = evtimage.ImageBase;
+            byte[] byBuffer = evtimage.ImageBase;
 
             // ALTERNATIVE - START
 
-            int size = 1228800;// evtimage.ImageBase.Length;
-            byte[] byBuffer = new byte[size];
+       //     int size = 1228800;// evtimage.ImageBase.Length;
+         //   byte[] byBuffer = new byte[size];
 
-            UnsafeNativeMethods.Copy(evtimage.ImageBase, 0, byBuffer, size, 100);
+           // UnsafeNativeMethods.Copy(evtimage.ImageBase, 0, byBuffer, size, 100);
 
             //            UnsafeHelper.Copy(
 
@@ -481,49 +481,49 @@ namespace TriggeredImageTakerApp
         }
     }
 
-    internal static class UnsafeNativeMethods
-    {
-        [DllImport("kernel32.dll", EntryPoint = "CopyMemory", SetLastError = false)]
-        public static extern void CopyMemory(IntPtr dest, IntPtr src, uint count);
+    //internal static class UnsafeNativeMethods
+    //{
+    //    [DllImport("kernel32.dll", EntryPoint = "CopyMemory", SetLastError = false)]
+    //    public static extern void CopyMemory(IntPtr dest, IntPtr src, uint count);
 
-        //[DllImport("kernel32.dll", EntryPoint = "RtlMoveMemory", ExactSpelling = true)]
-        //public static extern void CopyMemory(IntPtr dest, IntPtr src, IntPtr count);
+    //    //[DllImport("kernel32.dll", EntryPoint = "RtlMoveMemory", ExactSpelling = true)]
+    //    //public static extern void CopyMemory(IntPtr dest, IntPtr src, IntPtr count);
 
-        public static unsafe void Copy(byte[] source, int sourceOffset, byte[] target, int targetOffset, int count)
-        {
-            // If either array is not instantiated, you cannot complete the copy.
-            if ((source == null) || (target == null))
-            {
-                throw new System.ArgumentException();
-            }
+    //    public static unsafe void Copy(byte[] source, int sourceOffset, byte[] target, int targetOffset, int count)
+    //    {
+    //        // If either array is not instantiated, you cannot complete the copy.
+    //        if ((source == null) || (target == null))
+    //        {
+    //            throw new System.ArgumentException();
+    //        }
 
-            // If either offset, or the number of bytes to copy, is negative, you
-            // cannot complete the copy.
-            if ((sourceOffset < 0) || (targetOffset < 0) || (count < 0))
-            {
-                throw new System.ArgumentException();
-            }
+    //        // If either offset, or the number of bytes to copy, is negative, you
+    //        // cannot complete the copy.
+    //        if ((sourceOffset < 0) || (targetOffset < 0) || (count < 0))
+    //        {
+    //            throw new System.ArgumentException();
+    //        }
 
-            // If the number of bytes from the offset to the end of the array is
-            // less than the number of bytes you want to copy, you cannot complete
-            // the copy.
-            if ((source.Length - sourceOffset < count) ||
-                (target.Length - targetOffset < count))
-            {
-                throw new System.ArgumentException();
-            }
+    //        // If the number of bytes from the offset to the end of the array is
+    //        // less than the number of bytes you want to copy, you cannot complete
+    //        // the copy.
+    //        if ((source.Length - sourceOffset < count) ||
+    //            (target.Length - targetOffset < count))
+    //        {
+    //            throw new System.ArgumentException();
+    //        }
 
-            // The following fixed statement pins the location of the source and
-            // target objects in memory so that they will not be moved by garbage
-            // collection.
-            fixed (byte* pSource = source, pTarget = target)
-            {
-                // Copy the specified number of bytes from source to target.
-                for (int i = 0; i < count; i++)
-                {
-                    pTarget[targetOffset + i] = pSource[sourceOffset + i];
-                }
-            }
-        }
-    }
+    //        // The following fixed statement pins the location of the source and
+    //        // target objects in memory so that they will not be moved by garbage
+    //        // collection.
+    //        fixed (byte* pSource = source, pTarget = target)
+    //        {
+    //            // Copy the specified number of bytes from source to target.
+    //            for (int i = 0; i < count; i++)
+    //            {
+    //                pTarget[targetOffset + i] = pSource[sourceOffset + i];
+    //            }
+    //        }
+    //    }
+    //}
 }
